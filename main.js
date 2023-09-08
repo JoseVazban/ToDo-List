3c4f8c67fdff3c92cd346483a03123ff32fa7228
Jose Vazquez <jose.vazban@gmail.com>
2023-09-08 09:05:13 -0600

    ToDo List Servicio Social

---
+++ main.js
@@ -0,0 +1,30 @@
+let chores = []
+const choreInput = document.getElementById('chore');
+const choreList = document.getElementById("list1");
+
+function addButton() {
+    const choreText = choreInput.value.trim();
+    if (choreText.value != '') {
+        const li = document.createElement('li');
+        li.innerHTML = choreInput.value;
+        choreList.appendChild(li);
+        const btn = document.createElement('button');
+        btn.innerHTML = 'x';
+        li.appendChild(btn);
+    }
+    choreInput.value = "";
+    saveData();
+}
+
+choreList.addEventListener('click', function (e) {
+    e.target.parentElement.remove();
+    saveData();
+});
+
+function saveData(){
+    localStorage.setItem('data', choreList.innerHTML)
+}
+
+function showTask() {
+    choreList.innerHTML = localStorage.getItem('data')
+} showTask()

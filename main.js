760400d7eaf39783ba1aaa1f7a40d74b7eabb924
Jose Vazquez <jose.vazban@gmail.com>
2023-09-12 11:18:41 -0600

    More changes to the final product

--- main.js
+++ main.js
@@ -2,31 +2,41 @@ const choreInput = document.getElementById('chore');
 const choreList = document.getElementById("list1");
 
 function addButton() {
-    if (choreInput.value != '') {
+
+    var regEx = /^\S/;
+    if (choreInput.value.match (regEx)) {
         const li = document.createElement('li');
         li.innerHTML = choreInput.value;
         choreList.appendChild(li);
+
         const btn = document.createElement('button');
         btn.innerHTML = 'x';
         btn.classList.add('borrar');
         li.appendChild(btn);
-        li.classList.add('element')
+        li.classList.add('element');
+
+        choreInput.value = '';
+        saveData();
+
+        btn.addEventListener('click', function (e) {
+        e.target.parentElement.remove();
+        saveData();
+    });
+        
     } else {
-        alert ("Escribe algo por favor.");
+        alert('Escribe algo por favor.');
+        return;
     }
-    choreInput.value = "";
-    saveData();
+
 }
 
-choreList.addEventListener('click', function (e) {
-    e.target.parentElement.remove();
-    saveData();
-});
-
 function saveData(){
+
     localStorage.setItem('data', choreList.innerHTML)
+    
 }
 
 function showTask() {
+    
     choreList.innerHTML = localStorage.getItem('data')
 } showTask()
